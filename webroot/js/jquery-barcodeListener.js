710214ed3ef4456320296610a397d8a311201edb
Abdullah Zubair <abdullah.zubair@student.manchester.ac.uk>
2018-07-29 03:14:41 +0200

    Commit reset and add JS barcode scanner

--- webroot/js/jquery-barcodeListener.js
+++ webroot/js/jquery-barcodeListener.js
@@ -1,107 +1,47 @@
-/**
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- * This software consists of voluntary contributions made by many individuals
- * and is licensed under the new BSD license.
- *
- * @author      David Zeller <me@zellerda.com>
- * @license     http://www.opensource.org/licenses/BSD-3-Clause New BSD license
- * @version     1.0
- */
-(function($){
+var BarcodeScanner = function(options) {
+    this.initialize.call(this, options);
+};
 
-    $.barcodeListener = function(context, options){
-
-        var $defaults = {
-            support: [8, 12, 13]
-        };
-
-        var $this = this;
-        $this.element = $(context);
-        $this.timeout = 0;
-        $this.code = '';
-        $this.settings = {};
-
-        $this.init = function(){
-            $this.settings = $.extend({}, $defaults, options);
-            $this.element.on('keypress', function(e){
-                $this.listen(e);
-            })
-        };
-
-        $this.listen = function(e){
-            var $char = $this.validateKey(e.which);
-            if($char === 13){
-                $this.validate();
-            } else if($char !== false) {
-                if($this.code == ''){
-                    setTimeout($this.clear(), 1000);
-                }
-                $this.add($char);
-            }
-        };
-
-        $this.validate = function(){
-            var $tmp = $this.code;
-            if($this.settings.support.indexOf($tmp.length) > -1){
-                var $d = new Date(),
-                    $interval = $d.getTime() - $this.timeout;
-                $this.clear();
-                if($interval < 1000){
-                    $this.element.trigger('barcode.valid', [$tmp]);
-                }
-            } else {
-                $this.clear();
-            }
-        };
-
-        $this.clear = function(){
-            $this.code = '';
-            $this.timeout = 0;
-        };
-
-        $this.validateKey = function(keycode){
-            if(keycode == 13 || (keycode >= 48 && keycode <= 57)){
-                if(keycode == 13){
-                    return keycode;
-                } else {
-                    return String.fromCharCode(keycode);
-                }
-            } else {
-                return false;
-            }
-        };
-
-        $this.add = function(char){
-            if($this.timeout === 0){
-                var $d = new Date();
-                $this.timeout = $d.getTime();
-            }
-            $this.code += char;
-        };
-
-        $this.init();
-    };
-
-    $.fn.barcodeListener = function(options) {
-
-        return this.each(function(){
-            if(undefined == $(this).data('barcodeListener')){
-                var plugin = new $.barcodeListener(this, options);
-                $(this).data('barcodeListener', plugin);
-            }
+BarcodeScanner.prototype = {
+    initialize: function(options) {
+        $.extend(this._options,options);
+        $(document).on({
+            keyup: $.proxy(this._keyup, this)
         });
+    },
+    fire: function(str){
+        $(document).trigger('barcode',str);
+    },
+    _options: {timeout: 600, prefixKeyCode: 52, suffixKeyCode: 13, minKeyCode: 32, maxKeyCode: 126},
+    _isReading: false,
+    _timeoutHandler: false,
+    _inputString: '',
+    _keyup: function (e) {
+        if(this._isReading){
+            if(e.keyCode==this._options.suffixKeyCode){
+                //read end
+                if (this._timeoutHandler)
+                    clearTimeout(this._timeoutHandler);
+                this._isReading=false;
+                this.fire.call(this,this._inputString);
+                this._inputString='';
+            }else{
+                //char reading
+                if(e.which>=this._options.minKeyCode && e.which<=this._options.maxKeyCode)
+                    this._inputString += String.fromCharCode(e.which);
+            }
+        }else{
+            if(e.keyCode==this._options.prefixKeyCode){
+                //start reading
 
+                this._isReading=true;
+                this._timeoutHandler = setTimeout($.proxy(function () {
+                    //read timeout
+                    this._inputString='';
+                    this._isReading=false;
+                    this._timeoutHandler=false;
+                }, this), this._options.timeout);
+            }
+        }
     }
-
-})(jQuery);
+};
